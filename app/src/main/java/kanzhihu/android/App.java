965a038c09282d64e17b47a16a1344e406a81754
Jiahui.wen <jiahuiwen622111@gmail.com>
2014-11-11 20:45:30 +0800

    添加ActivityCallback. 对activity的生命周期进行监听。

--- app/src/main/java/kanzhihu/android/App.java
+++ app/src/main/java/kanzhihu/android/App.java
@@ -1,7 +1,9 @@
 package kanzhihu.android;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import kanzhihu.android.managers.ActivityManager;
 
 /**
  * Created by Jiahui.wen on 2014/11/6.
@@ -14,9 +16,24 @@ public class App extends Application {
         return mApplicationContext;
     }
 
+    private ActivityManager mCallBack;
+
     @Override public void onCreate() {
         super.onCreate();
 
+        mCallBack = new ActivityManager();
+        registerActivityLifecycleCallbacks(mCallBack);
+
         mApplicationContext = this.getApplicationContext();
+    }
+
+    public Activity topActivity() {
+        return mCallBack.topActivity();
+    }
+
+    @Override public void onTerminate() {
+        super.onTerminate();
+        unregisterActivityLifecycleCallbacks(mCallBack);
+        mCallBack.gc();
     }
 }
